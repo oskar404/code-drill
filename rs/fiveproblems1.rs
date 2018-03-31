b75994319fba4f1c2719481441b00d3bd948348f
Oskar Lönnberg <k.oskar.lonnberg@gmail.com>
2018-03-31 23:03:35 +0300

    5 Problems: Rust solutions for 1
    When you get the hang of it the Rust code is kind of elegant

---
+++ rs/fiveproblems1.rs
@@ -0,0 +1,34 @@
+// Write three functions that compute the sum of the numbers in a given list
+// using a for-loop, a while-loop, and recursion.
+
+fn main() {
+    let l1: [i32; 10] = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34];
+    println!("forloop(): {}", forloop(&l1));
+    println!("whileloop(): {}", whileloop(&l1));
+    println!("recursion(): {}", recursion(&l1, 0));
+}
+
+fn forloop(list: &[i32]) -> i32 {
+    let mut sum = 0;
+    for i in list.iter() {
+        sum += i;
+    }
+    sum
+}
+
+fn whileloop(list: &[i32]) -> i32 {
+    let mut sum = 0;
+    let mut i = 0;
+    while i < list.len() {
+        sum += list[i];
+        i += 1;
+    }
+    sum
+}
+
+fn recursion(list: &[i32], i: usize) -> i32 {
+    if i >= list.len() {
+        return 0;
+    }
+    return list[i] + recursion(list, i+1);
+}
