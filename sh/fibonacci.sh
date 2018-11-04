ece9224c382217c327c5679ee7d6e7c67b9bc051
Oskar Lönnberg <k.oskar.lonnberg@gmail.com>
2018-11-04 14:55:12 +0200

    sh: Fix linter problems
    Fix problems found by shellcheck linter for fibonacci.sh script.
    The fixed linter errors:
    
    - SC2086: Double quote to prevent globbing and word splitting.
    - SC2004: $ on variables in (( )) is unnecessary.

--- sh/fibonacci.sh
+++ sh/fibonacci.sh
@@ -9,26 +9,26 @@ function fibonacci()
     local i1=0
     local i2=1
     local i=2
-    [ $1 == 0 ] && return
+    [[ $1 == 0 ]] && return
     echo $i1
-    [ $1 == 1 ] && return
+    [[ $1 == 1 ]] && return
     echo $i2
-    [ $1 == 2 ] && return
-    while [ $i -lt $1 ]; do
-        local i3=$(($i1 + $i2))
-        [ $i3 -lt $i2 ] && (>&2 echo "Overflow at: $i") && return
+    [[ $1 == 2 ]] && return
+    while [[ $i -lt $1 ]] ; do
+        local i3=$((i1 + i2))
+        [[ $i3 -lt $i2 ]] && (>&2 echo "Overflow at: $i") && return
         echo $i3
         i1=$i2
         i2=$i3
-        i=$(($i+1))
+        i=$((i+1))
     done
 }
 
 
 USAGE="usage: fibonacci.sh <count>"
-[ "$1" == "-h" ] && (>&2 echo $USAGE) && exit 1
+[[ $1 == "-h" ]] && (>&2 echo "$USAGE") && exit 1
 COUNT=100
-[ -n "$1" ] && COUNT=$1
-[ -z "${COUNT##*[!0-9]*}" ] && (>&2 echo "Not a positive number: $1") && exit 1
+[[ -n $1 ]] && COUNT=$1
+[[ -z ${COUNT##*[!0-9]*} ]] && (>&2 echo "Not a positive number: $1") && exit 1
 
-fibonacci $COUNT
+fibonacci "$COUNT"
